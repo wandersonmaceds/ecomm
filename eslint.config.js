ff41d0ab4623f03af7b82059e780fb88735da10c
Wanderson M. <wandersonmaceds@gmail.com>
2024-09-24 15:33:03 -0300

    adding husky, eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@ import pluginJs from "@eslint/js";
 
 export default [
   { languageOptions: { globals: globals.node } },
-  { ignores: ['node_modules'] },
+  { ignores: ["node_modules"] },
   pluginJs.configs.recommended,
 ];
