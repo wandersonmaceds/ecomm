359f41453ac514e8e6cc6fe8ee37f9d774ad9aed
Wanderson M. <wandersonmaceds@gmail.com>
2024-09-24 15:22:32 -0300

    adding healthcheck test

---
+++ test/health.test.js
@@ -0,0 +1,13 @@
+import assert from 'node:assert/strict'
+import { describe, it } from 'node:test';
+
+import app from '../src/main.js';
+import agent from 'supertest';
+
+describe('App', () => {
+    it('should response with success with status running', (_, done) => {
+        agent(app)
+            .get('/health')
+            .expect(200, { status: 'running' }, done);
+    });
+})
--- test/testing.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'node:assert/strict'
-import { describe, it } from 'node:test';
-
-
-describe('Test', () => {
-    it('should pass without errors', async () => {
-        assert.strictEqual(4 + 4, 8);
-    });
-})
